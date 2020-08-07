18e3d0ab5f41fec584a8036797e7eb6459556107
Dubz <Dubz@users.noreply.github.com>
2020-08-07 04:36:58 -0400

    Update request.php
    Moved access control headers to top of script for global use.
    Added scan tool responses now, because apparently people feel the need to use exploitation scanners on this static content site...

--- request.php
+++ request.php
@@ -10,6 +10,10 @@ define("SUBDOMAIN", implode('.', array_slice(explode('.', $_SERVER['SERVER_NAME'
 // $req = array_key_exists('req', $_GET) ? $_GET['req'] : 'index.html';
 $req = substr($_SERVER['SCRIPT_URL'], 1) or false;
 
+# This seemed relatively important, so we're gonna put it up here.
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET');
+
 /*
  * networkcheck.xyz currently has a bad reputation,
  * due to hosting lnkr5.min.js adware. This block of
@@ -31,9 +35,20 @@ if(HONEYPOT_RUNNING)
 	if(SUBDOMAIN === 'ph' || SUBDOMAIN === 'ph.dev')
 	{
 		header("HTTP/1.1 200 OK");
-		header('Access-Control-Allow-Origin: *');
-		header('Access-Control-Allow-Methods: GET');
 		header('Access-Control-Max-Age: 0');
+		die();
+	}
+	if(in_array($_SERVER['HTTP_USER_AGENT'],
+		array(
+			'Nuclei - Open-source project (github.com/projectdiscovery/nuclei)'
+		)
+	)
+	|| stripos($_SERVER['HTTP_USER_AGENT'], 'passwd')
+	)
+	{
+		header('HTTP/1.1 402 Payment Required');
+		header('Access-Control-Max-Age: 0');
+		print array_rand(array('Yeah, this is pretty much a guaranteed above PG rating...', 'Oh yeah', 'Oooooh', 'That feels so good', 'Yesss', 'Harder daddy', 'Paint me like one of your french bears', 'If at first you don\'t succeed, go play in traffic.', 'We all know you\'re touching yourself...', 'It\'s okay to feel sad sometimes.', 'You won!', 'lol', 'You thought.', 'Try again later', 'Try again later', 'Try again later', 'Try again later', 'You owe me $20', 'Tell your mom I\'m sorry about busting her headboard.', 'Try getting some fresh air.', 'Thank you for keeping your distance during these times.', 'Please stay at least 6 feet apart.' 'The cake is a lie', 'This response is brought to you by: DEEZ NUTS', 'Ha, gotem.', 'You know {insert pathetic wannabe hacker\'s name}, sometimes...I don\'t believe I know you...', 'Show me your tits', 'Send it', 'You can do it!', 'You\'re almost there...', 'Did you try turning it off and back on again??', 'If you need some help, please hang up and call the operator.', 'Dude where\'s my car?', 'iykyk', 'Tiktok is literally cancer.', 'How much wood coulda woodchuck chuck, if a woodchuck could chuck wood?', 'A woodchuck could chuck as much wood as a woodchuck could chuck, if a woodchuck could chuck wood.', 'Hey you dang woodtouch, quit touching your wood!', 'You seem like the kind of person who gets off on Miley Cyrus when she came in on a wrecking ball.', 'Yo dawg I heard your lookin for vulns?', 'Are you *seriously* still here? The entire source for this folder is literally on Github...', 'Did you even try reading the page\'s HTML comments, or are you that special kinda retarded?'));
 		die();
 	}
 }
